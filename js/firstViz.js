3f5adbfac441259a77c5bf9d63b665b7427d90e1
LAFIOSCA LEO p1609209 <leo.lafiosca@etu.univ-lyon1.fr>
2019-01-09 17:43:42 +0100

    Animated line chart

--- js/firstViz.js
+++ js/firstViz.js
@@ -1,33 +1,71 @@
 
-function makeChart(svg, nb)
+function makeChart(svg)
 {
-    var	margin = {top: 0, right: 10, bottom: 40, left: 20},
-	width = 600 - margin.left - margin.right,
-	height = 520 - margin.top - margin.bottom;
+
+    var margin = {top: 0, right: 10, bottom: 40, left: 20},
+        width = 500 - margin.left - margin.right,
+        height = 520 - margin.top - margin.bottom;
 
     // Parse the date / time
-    var	parseDate = d3.time.format("%d-%b-%y").parse;
+    var parseDate = d3.time.format("%Y").parse;
 
     // Set the ranges
-    var	x = d3.time.scale().range([0, width]);
-    var	y = d3.scale.linear().range([height, 0]);
+    var x = d3.time.scale().range([0, width]);
+    var y = d3.scale.linear().range([height, 0]);
 
     // Define the axes
-    var	xAxis = d3.svg.axis().scale(x)
-	.orient("bottom").ticks(5);
+    var xAxis = d3.svg.axis().scale(x)
+        .orient("bottom").ticks(5);
 
-    var	yAxis = d3.svg.axis().scale(y)
-	.orient("left").ticks(5);
+    var yAxis = d3.svg.axis().scale(y)
+        .orient("left").ticks(5);
 
     // Define the line
-    var	valueline = d3.svg.line()
-	.x(function(d) { return x(d.date); })
-	.y(function(d) { return y(d.TWh); });
- 
+    var valueline = d3.svg.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.TWh); });
+    
     // Get the data
     d3.csv("./data/data.csv", function(error, data) {
-        for (var i = 0; i<nb; i++) {
-            console.log(data[i])
+        for (var i = 0; i<26; i++) {
+	    data[i].date = parseDate(data[i].date);
+	    data[i].TWh = +data[i].TWh;
+	};
+
+	// Scale the range of the data
+	x.domain(d3.extent(data, function(d) { return d.date; }));
+	y.domain([0, d3.max(data, function(d) { return d.TWh; })]);
+
+	// Add the valueline path.
+	var pathLineChart = svg.append("path")
+	    .attr("class", "lineChart")
+	    .attr("d", valueline(data))
+            .style("opacity", "0");
+
+	// Add the X Axis
+	svg.append("g")
+	    .attr("class", "x leoLine")
+	    .attr("transform", "translate(0," + height + ")")
+	    .call(xAxis);
+
+	// Add the Y Axis
+	svg.append("g")
+	    .attr("class", "y leoLine")
+	    .call(yAxis);
+
+        pathLineChart.transition()
+            .duration(2000)
+            .style("opacity", "1");
+    });
+}
+
+
+
+function addStep(nbStep)
+{
+    // Get the data
+    d3.csv("./data/data.csv", function(error, data) {
+        for (var i = 0; i<1; i++) {
 	    data[i].date = parseDate(data[i].date);
 	    data[i].TWh = +data[i].TWh;
 	};
@@ -51,6 +89,5 @@ function makeChart(svg, nb)
 	svg.append("g")
 	    .attr("class", "y leoLine")
 	    .call(yAxis);
-
     });
 }
